a23c15093b48a490154b434f8e09f533632717bf
Krzysztof Raciniewski <raciniak@gmail.com>
2014-03-20 10:22:41 -0700

    Dodałem dwie klasy wyjątków, które wyrzucane są z klasy VideoThingumebob

---
+++ VideoThingumebob/src/com/lecturershow/videothingumebob/ExceptionClasses/BadDirectoryLocationException.java
@@ -0,0 +1,11 @@
+package com.lecturershow.videothingumebob.ExceptionClasses;
+
+public class BadDirectoryLocationException extends Exception {
+
+	private static final long serialVersionUID = -5485423837851739560L;
+
+	public BadDirectoryLocationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ VideoThingumebob/src/com/lecturershow/videothingumebob/ExceptionClasses/BadTypeFileException.java
@@ -0,0 +1,12 @@
+package com.lecturershow.videothingumebob.ExceptionClasses;
+
+public class BadTypeFileException extends Exception {
+
+	private static final long serialVersionUID = 8257768769290571829L;
+
+	public BadTypeFileException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
